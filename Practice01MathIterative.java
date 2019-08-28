da43e1f8c7ea9d0c1026abd3dd87d6b92dcdc60c
chia <aesparzachavez@dons.usfca.edu>
2019-08-28 13:10:29 -0700

    finished

--- Practice01MathIterative.java
+++ Practice01MathIterative.java
@@ -1,20 +1,27 @@
 public class Practice01MathIterative implements Practice01Math {
 
-    public int fib(int n) {
+    public int fib(int n) throws Exception {
+        if (n < 0)
+            throw new Exception();
+
+
         int fibonacci = 0;
-        int x = 0;
+        int x = 1;
         if (n == 0 || n == 1)
             return n;
         else
-            for (int i = 0; i <= n; i++){
+            for (int i = 0; i < n; i++) {
+                int y = fibonacci;
                 fibonacci += x;
-                x = fibonacci;
+                x = y;
             }
-            return x;
-
+        return fibonacci;
     }
 
-    public int fact(int n) {
+    public int fact(int n) throws Exception {
+        if (n < 0)
+            throw new Exception();
+
         int factorial = 1;
         if (n == 0 || n == 1)
             return n;
@@ -22,6 +29,6 @@ public class Practice01MathIterative implements Practice01Math {
             for (int i = 2; i <= n; i++) {
                 factorial *= i;
             }
-            return factorial;
-    }
+        return factorial;
+}
 }
--- Practice01MathRecursive.java
+++ Practice01MathRecursive.java
@@ -1,17 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Practice01MathRecursive implements Practice01Math {
 
-    public int fib(int n){
-        if (n == 0 || n == 1)
+    public int fib(int n) throws Exception{
+        if (n < 0)
+            throw new Exception();
+        else if (n == 0 || n == 1)
             return n;
         else
             return fib(n-1) + fib(n-2);
     }
 
-    public int fact(int n){
-        if (n == 0 || n == 1)
+    public int fact(int n) throws Exception {
+        if (n < 0)
+            throw new Exception();
+        else if (n == 0 || n == 1)
             return n;
         else
-            return n * fact(n-1);
+            return n * fact(n - 1);
     }
-
 }
